fee36d760c758a289efc6b1dcb4329fa50638a74
Andreas Olsson <olsson.andreas@gmail.com>
2021-07-03 07:50:30 +0200

    feat(React router dom): React router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import DockerImages from "./components/docker-images-component/DockerImages";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Link to="/">Home</Link>
+
+      <Route path="/" exact component={DockerImages} />
+    </Router>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
-      <DockerImages />
+      <AppRouter />
     </div>
   );
 }
