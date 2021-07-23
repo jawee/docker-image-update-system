9676c74796261df07d78aaf27fc48f648bf8dd1d
Andreas Olsson <olsson.andreas@gmail.com>
2021-07-23 11:37:51 +0200

    feat(Create Image) Can now create and redirects to the new image

--- src/components/docker-image-create/DockerImageCreate.tsx
+++ src/components/docker-image-create/DockerImageCreate.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import DockerImage from "./../../models/DockerImage";
+import DockerImage from "../../models/DockerImage";
+import DockerImageService from "../../services/docker-image-service";
+import { useHistory } from "react-router-dom";
 
 const DockerImageCreate = () => {
-  const [dockerImage, setDockerImage] = React.useState<DockerImage>(
-    {} as DockerImage
-  );
+  const [dockerImage, setDockerImage] = React.useState<DockerImage>({
+    status: 1,
+    repository: "docker",
+  } as DockerImage);
+
+  const selectOnChange = (event: React.FormEvent<HTMLSelectElement>) => {
+    let key = event.currentTarget.name;
+    let value = event.currentTarget.value;
+
+    setDockerImage({ ...dockerImage, [key]: value });
+  };
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     let key = event.currentTarget.name;
     let value =
@@ -13,21 +23,18 @@ const DockerImageCreate = () => {
           ? 1
           : 0
         : event.currentTarget.value;
-    let currentDockerImage = dockerImage;
-    currentDockerImage[key] = value;
     setDockerImage({ ...dockerImage, [key]: value });
   };
+  let history = useHistory();
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    /*const service: DockerImageService = new DockerImageService();
-    const updateDockerImage = async () => {
-      await service.updateDockerImage(+id, dockerImage);
-      //  setDockerImage(res);
-      //  setLoading(false);
+    const service: DockerImageService = new DockerImageService();
+    const createDockerImage = async () => {
+      const newImage = await service.createDockerImage(dockerImage);
       //  Redirect to details view
-      history.push("/dockerimage/" + dockerImage.id);
-      };
-      updateDockerImage();*/
+      history.push("/dockerimage/details/" + newImage.id);
+    };
+    createDockerImage();
   };
 
   return (
@@ -39,7 +46,10 @@ const DockerImageCreate = () => {
       </label>
       <label htmlFor="repository">
         Repository
-        <input name="repository" type="text" onChange={onChange} />
+        <select name="repository" onChange={selectOnChange}>
+          <option value="docker">Docker</option>
+          <option value="github">Github</option>
+        </select>
       </label>
       <label htmlFor="user">
         User
