2eb5d2969c2b8ff320dd17f64498f1aa61263ca1
Andreas Olsson <olsson.andreas@gmail.com>
2021-07-26 12:45:10 +0200

    api: test commit

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { Server } from "./server";
 import { createConnection } from "typeorm";
-
+console.log("test");
 createConnection()
   .then(async () => {
     const server = new Server();
